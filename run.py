5a289f675b72dd229f6c81b3d39577d57f759dac
moscarellar <moscarellar@gmail.com>
2022-05-30 10:48:39 +0000

    description for def define_word include

--- run.py
+++ run.py
@@ -2,6 +2,12 @@ import random
 from word_listing import words
 
 def define_word():
+     """
+    Gets word from word_listing 
+    and returns it upper case
+    """
     word = random.choice(words)
     return word.upper()
+
+
 

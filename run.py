184e00d568a66f22272587b6d7493cd5e1fa8add
moscarellar <moscarellar@gmail.com>
2022-05-30 14:06:49 +0000

    wordPrintUpdate function to reprint word

--- run.py
+++ run.py
@@ -9,6 +9,8 @@ def define_word():
     word = random.choice(words)
     return word.upper()
 
+
+
 def hangman_image(error):
     """
     If/elif to
@@ -56,3 +58,25 @@ def hangman_image(error):
         print("/|\  |")
         print("/ \  |")
         print("    ===")
+
+
+### Def that will update word and print it
+def wordPrintUpdate(lettersGuessed):
+    times=0
+    rightLetters=0
+    for char in word:
+        if(char in lettersGuessed):
+            print(randomWord[times], end=" ")
+            rightLetters+=1
+        else:
+            print("_", end=" ")
+            times+=1
+    return rightLetters
+
+
+def hangman(word):
+    complete_word = "_" * len(word)
+    lettersGuessed = []
+    error = 0
+    print("Welcome to Hangman!")
+    print(hangman_image(error))

e56866864d49ce346daf540353728773c4e5fd8f
moscarellar <moscarellar@gmail.com>
2022-05-30 10:57:44 +0000

    def hangman_image that display image based on error count

--- run.py
+++ run.py
@@ -9,5 +9,50 @@ def define_word():
     word = random.choice(words)
     return word.upper()
 
-
-
+def hangman_image(error):
+    """
+    If/elif to
+    Prints image based on error count
+    """
+    if (error == 0):
+        print("\n+---+")
+        print("    |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif (error == 1):
+        print("\n+---+")
+        print("O   |")
+        print("    |")
+        print("    |")
+        print("   ===")
+    elif (error == 2):
+        print("\n+---+")
+        print("O   |")
+        print("|   |")
+        print("    |")
+        print("   ===")
+    elif (error == 3):
+        print("\n+---+")
+        print(" O  |")
+        print("/|  |")
+        print("    |")
+        print("   ===")
+    elif (error == 4):
+         print("\n+---+")
+        print(" O  |")
+        print("/|\ |")
+        print("    |")
+        print("   ===")
+    elif (error == 5):
+        print("\n+---+")
+        print(" O  |")
+        print("/|\ |")
+        print("/   |")
+        print("   ===")
+    elif (error == 6):
+        print("\n+---+")
+        print(" O   |")
+        print("/|\  |")
+        print("/ \  |")
+        print("    ===")

a8f4262badf040987516ba78f247a266622234b8
moscarellar <moscarellar@gmail.com>
2022-05-30 10:14:26 +0000

    words py document creation for words selection

---
+++ words.py
@@ -0,0 +1,16 @@
+words = [
+    "Amazing",
+    "Panama",
+    "Taxi",
+    "Spanish",
+    "English",
+    "Videos",
+    "Games",
+    "Films",
+    "Schopenhauer",
+    "Marx",
+    "Amsterdam",
+    "Good",
+    "Countries",
+    "Flights"
+]
